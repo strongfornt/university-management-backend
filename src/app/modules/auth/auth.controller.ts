8551fee1899b4dd900268b0dc5bb635a79e7c1bc
storngfornt <au0038173@gmail.com>
2024-12-21 20:54:08 +0600

    refresh token added in cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,15 +2,24 @@ import { JwtPayload } from "jsonwebtoken";
 import catchAsync from "../../utils/catch-async";
 import sendResponse from "../../utils/sendResponse";
 import { AuthServices } from "./auth.service";
+import config from "../../config";
 
 const loginUser = catchAsync(async (req, res, next) => {
     const {body} = req.body;
     const result = await AuthServices.loginUser(body) 
+    const {refreshToken, accessToken, needsPasswordChange} = result;
+    res.cookie('refreshToken', refreshToken, {
+        secure: config.NODE_ENV === 'production',
+        httpOnly: true
+    });
     sendResponse(res, {
         statusCode: 200,
         success: true,
         message: 'User is logged in successfully',
-        data: result,
+        data: {
+            accessToken,
+            needsPasswordChange
+        },
       });
 })
 const changePassword = catchAsync(async (req, res, next) => {
@@ -20,7 +29,7 @@ const changePassword = catchAsync(async (req, res, next) => {
         statusCode: 200,
         success: true,
         message: 'Password is updated successfully',
-        data: result,
+        data: result
       });
 })
 
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,8 +3,9 @@ import AppError from '../../errors/AppError';
 import { UserModel } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 import bcrypt from 'bcrypt';
-import jwt, { JwtPayload } from 'jsonwebtoken'
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../../config';
+import { createToken } from './auth.utils';
 
 const loginUser = async (payload: TLoginUser) => {
   // checking if user exists
@@ -16,16 +17,16 @@ const loginUser = async (payload: TLoginUser) => {
   }
 
   //checking if the user already deleted
-  const isDeleted = isUserExists?.isDeleted
+  const isDeleted = isUserExists?.isDeleted;
 
-  if(isDeleted) {
-      throw new AppError(StatusCodes.FORBIDDEN, 'This user is deleted!')
+  if (isDeleted) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'This user is deleted!');
   }
   //checking if the user status blocked
   const userStatus = isUserExists?.status;
 
-  if(userStatus === 'blocked') {
-      throw new AppError(StatusCodes.FORBIDDEN, 'This user is blocked!');
+  if (userStatus === 'blocked') {
+    throw new AppError(StatusCodes.FORBIDDEN, 'This user is blocked!');
   }
 
   // // checking if the password is correct
@@ -41,23 +42,33 @@ const loginUser = async (payload: TLoginUser) => {
   // console.log(isPasswordCorrect);
 
   //create token and send to the client
-const jwtPayload =  {
-  userId: isUserExists?.id,
-  role: isUserExists?.role,
-}
+  const jwtPayload = {
+    userId: isUserExists?.id,
+    role: isUserExists?.role,
+  };
 
-const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, { expiresIn: '10d' });
+  const accessToken = createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as string,
+  );
+  const refreshToken = createToken(
+    jwtPayload,
+    config.jwt_refresh_secret as string,
+    config.jwt_refresh_expires_in as string,
+  );
 
   return {
     accessToken,
-    needsPasswordChange: isUserExists?.needsPasswordChange
+    refreshToken,
+    needsPasswordChange: isUserExists?.needsPasswordChange,
   };
 };
 
-
-
-
-const changePassword = async (userData: JwtPayload, payload:{oldPassword: string; newPassword: string}) => {
+const changePassword = async (
+  userData: JwtPayload,
+  payload: { oldPassword: string; newPassword: string },
+) => {
   const isUserExists = await UserModel.isUserExistsByCustomId(userData?.userId);
 
   if (!isUserExists) {
@@ -65,16 +76,16 @@ const changePassword = async (userData: JwtPayload, payload:{oldPassword: string
   }
 
   //checking if the user already deleted
-  const isDeleted = isUserExists?.isDeleted
+  const isDeleted = isUserExists?.isDeleted;
 
-  if(isDeleted) {
-      throw new AppError(StatusCodes.FORBIDDEN, 'This user is deleted!')
+  if (isDeleted) {
+    throw new AppError(StatusCodes.FORBIDDEN, 'This user is deleted!');
   }
   //checking if the user status blocked
   const userStatus = isUserExists?.status;
 
-  if(userStatus === 'blocked') {
-      throw new AppError(StatusCodes.FORBIDDEN, 'This user is blocked!');
+  if (userStatus === 'blocked') {
+    throw new AppError(StatusCodes.FORBIDDEN, 'This user is blocked!');
   }
 
   // // checking if the password is correct
@@ -88,25 +99,28 @@ const changePassword = async (userData: JwtPayload, payload:{oldPassword: string
     throw new AppError(StatusCodes.FORBIDDEN, 'Password do not matched');
   }
 
+  // hash new password before update
+  const newHashedPassword = await bcrypt.hash(
+    payload?.newPassword,
+    Number(config.bcrypt_salt_round),
+  );
 
-// hash new password before update
-const newHashedPassword = await bcrypt.hash(payload?.newPassword, Number(config.bcrypt_salt_round))
-
-   await UserModel.findOneAndUpdate({
-    id: userData?.userId,
-    role: userData?.role,
-  }, {
-    password:newHashedPassword,
-    needsPasswordChange: false,
-    passwordChangeAt: new Date(),
-  })
-
+  await UserModel.findOneAndUpdate(
+    {
+      id: userData?.userId,
+      role: userData?.role,
+    },
+    {
+      password: newHashedPassword,
+      needsPasswordChange: false,
+      passwordChangeAt: new Date(),
+    },
+  );
 
   return null;
-
-}
+};
 
 export const AuthServices = {
   loginUser,
-  changePassword
+  changePassword,
 };
